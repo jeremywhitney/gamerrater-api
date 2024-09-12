f2ca0103ee82f62c8be5a38ce63adeb44a25fc78
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-12 15:05:40 -0500

    Created models for 'category', 'game', 'game_category', 'picture' and 'review'. Updated 'settings' with media file settings to use ImageField, and updated package with imports.

---
+++ raterapi/models/game.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    designer = models.CharField(max_length=255)
+    year_released = models.IntegerField()
+    number_of_players = models.IntegerField()
+    estimated_time_to_play = models.IntegerField()
+    age_recommendation = models.CharField(max_length=50)
+    categories = models.ManyToManyField(
+        "Category", through="GameCategory", related_name="games"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
