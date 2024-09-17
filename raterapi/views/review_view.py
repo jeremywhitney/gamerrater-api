35bdbdedeb6ecfb800382a63b97639fd0c190e92
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-17 15:51:13 -0500

    Updated 'ReviewViewSet' queryset to look for game_id so when the fetch call is made from the client only reviews from the currently viewed game are displayed

--- raterapi/views/review_view.py
+++ raterapi/views/review_view.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers, viewsets
 from raterapi.models import Review
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name"]
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    player = UserSerializer(read_only=True)
+
     class Meta:
         model = Review
         fields = [
@@ -17,5 +26,12 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all().order_by("-created_at")
     serializer_class = ReviewSerializer
+    queryset = Review.objects.all().order_by("-created_at")
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        game_id = self.request.query_params.get("gameId")
+        if game_id:
+            queryset = queryset.filter(game_id=game_id)
+        return queryset
