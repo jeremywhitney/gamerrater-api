0cc216a2b57b20d442ba982f05d18c920675bd81
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-20 13:47:55 -0500

    Modified 'Picture' model and edited 'settings' to allow image uploads.

--- raterapi/models/picture.py
+++ raterapi/models/picture.py
@@ -3,7 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Picture(models.Model):
-    image = models.URLField()
-    game = models.ForeignKey("Game", on_delete=models.CASCADE)
-    player = models.ForeignKey(User, on_delete=models.CASCADE)
-    uploaded_at = models.DateTimeField(auto_now_add=True, null=True)
+    game = models.ForeignKey(
+        "Game", on_delete=models.DO_NOTHING, related_name="pictures"
+    )
+    action_pic = models.ImageField(
+        upload_to="actionimages",
+        height_field=None,
+        width_field=None,
+        max_length=None,
+        null=True,
+    )
