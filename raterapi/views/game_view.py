7d29b27e1486b7ab508822482b2203a920d4b84b
Jeremy Whitney <bearwhitness@gmail.com>
2024-09-22 16:30:36 -0500

    Updated 'game_view' list method to include order_by

--- raterapi/views/game_view.py
+++ raterapi/views/game_view.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from raterapi.models import Game, Category
 from django.contrib.auth.models import User
-from django.db.models import Q
+from django.db.models import F, Q
 
 
 class GameUserSerializer(serializers.ModelSerializer):
@@ -70,13 +70,26 @@ class GameViewSet(viewsets.ModelViewSet):
             game.save()
 
     def list(self, request):
-        search_text = self.request.query_params.get("q", None)
         queryset = self.queryset
+        search_text = self.request.query_params.get("q", None)
+        order_by = self.request.query_params.get("orderby", None)
+
         if search_text is not None:
             queryset = queryset.filter(
                 Q(title__icontains=search_text)
                 | Q(description__icontains=search_text)
                 | Q(designer__icontains=search_text)
             )
+
+        if order_by:
+            if order_by == "year":
+                queryset = queryset.order_by(F("year_released").desc(nulls_last=True))
+            elif order_by == "time":
+                queryset = queryset.order_by(
+                    F("estimated_time_to_play").desc(nulls_last=True)
+                )
+            elif order_by == "designer":
+                queryset = queryset.order_by("designer")
+
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
